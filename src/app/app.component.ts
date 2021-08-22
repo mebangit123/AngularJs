3fbb857ce33a4f7eb6d4bcf0ad9852df9e78e8c1
mebangit123 <mebannongrum333@gmail.com>
2021-08-22 11:18:02 +0530

    Created HelloWorld Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title = "Hello World";
 }
