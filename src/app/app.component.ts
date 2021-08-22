a14d67e6b3da6635dce7060c3127151aadd788b3
mebangit123 <mebannongrum333@gmail.com>
2021-08-22 12:37:06 +0530

    Refractor: To ensure the username is valid input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   imgUrl = "../assets/image/bridgelabz.png";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz."
@@ -19,5 +20,15 @@ export class AppComponent {
   onClick($event) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!"
   }
 }
