d1b281ce88ef1cfe01e9c02812edebc2953884c9
mebangit123 <mebannongrum333@gmail.com>
2021-08-22 12:00:53 +0530

    Refractor: Use Event Binding to bind Click Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Hello World";
   imgUrl = "../assets/image/bridgelabz.png"
+  url = "https://www.bridgelabz.com"
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz."
+  }
+
+  onClick($event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
